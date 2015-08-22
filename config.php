a72e7e09da85d37f46f95224ed08d0c72f63e69d
Bil Simser <emailme@bilsimser.com>
2015-08-21 19:00:36 -0600

    updated config.php with some instructions

--- config.php
+++ config.php
@@ -1,10 +1,14 @@
 <?php // config.php :: Low-level app/database variables.
 
-// done this way so we don't check in our passwords into github
+// this is for local testing for remote create a file named local.config.php based on blank.config.php
+// if you don't want to create a new file and are not publishing this anywhere you can just edit it directly
+// this file has been modified this way so we don't check in our passwords into github
+// see Mark Jaquith's post about hiding database connections for WordPress which this is based on
+// https://markjaquith.wordpress.com/2011/06/24/wordpress-local-dev-tips/
+
 if(file_exists(dirname(__FILE__) . '/local.config.php')) {
         include(dirname(__FILE__) . '/local.config.php');
 } else {
-    // this is for local testing for remote create a file named local.config.php based on blank.config.php
     $dbsettings = Array(
         "server"        => "localhost",     // MySQL server name. (Default: localhost)
         "user"          => "",              // MySQL username.
