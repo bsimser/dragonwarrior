fbf38d5c138c67c1f5aa8d108b3ef82252f7f51d
Bil Simser <emailme@bilsimser.com>
2015-08-21 18:07:27 -0600

    adding initial version

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php // config.php :: Low-level app/database variables.
+
+$dbsettings = Array(
+        "server"        => "localhost",     // MySQL server name. (Default: localhost)
+        "user"          => "simstool_dk",              // MySQL username.
+        "pass"          => "Silas551",              // MySQL password.
+        "name"          => "simstool_dk",              // MySQL database name.
+        "prefix"        => "dk",            // Prefix for table names. (Default: dk)
+        "secretword"    => "abracadabra");             // Secret word used when hashing information for cookies.
+
+?>
