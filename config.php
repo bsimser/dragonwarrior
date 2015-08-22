b18aee2dd579f9675972fd5952c8c98988d6b9af
Bil Simser <emailme@bilsimser.com>
2015-08-21 18:31:33 -0600

    * removed thumbnail file * modified config to user non-versioned file * updated gitignore to skip config file

--- config.php
+++ config.php
@@ -1,11 +1,16 @@
 <?php // config.php :: Low-level app/database variables.
 
-$dbsettings = Array(
-        "server"        => "localhost",     // MySQL server name. (Default: localhost)
-        "user"          => "simstool_dk",              // MySQL username.
-        "pass"          => "Silas551",              // MySQL password.
-        "name"          => "simstool_dk",              // MySQL database name.
-        "prefix"        => "dk",            // Prefix for table names. (Default: dk)
-        "secretword"    => "abracadabra");             // Secret word used when hashing information for cookies.
+// done this way so we don't check in our passwords into github
+if(file_exists(dirname(__FILE__) . '/local.config.php')) {
+        include(dirname(__FILE__) . '/local.config.php');
+} else {
+        $dbsettings = Array(
+            "server"        => "localhost",     // MySQL server name. (Default: localhost)
+            "user"          => "MySqlUser",              // MySQL username.
+            "pass"          => "MySqlPassword",              // MySQL password.
+            "name"          => "MySqlDatabase",              // MySQL database name.
+            "prefix"        => "dk",            // Prefix for table names. (Default: dk)
+            "secretword"    => "");             // Secret word used when hashing information for cookies.
+}
 
 ?>
